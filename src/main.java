1624f53cff1de2ea76a02fba1064ff3c22d24241
Matheus <mfshops01@gmail.com>
2024-08-28 22:44:03 -0300

    Aula sobre debbuging sobre pilha de execução de um programa java.

---
+++ src/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Iniciou do programa no método main.");
+        a();
+        System.out.println("Finalizou do programa no método main");
+    }
+
+    static void a(){
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a.");
+    }
+
+    static void b(){
+        System.out.println("Entrou no método b.");
+        for(int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+
+    static void c(){
+        System.out.println("Entrou no metodo c.");
+        Thread.dumpStack();
+    }
+}
